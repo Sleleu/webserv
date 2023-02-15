ea8fc563bf0a7fbeb43ccc5db27ef744a7af2034
GMansuy <gabriel.mansuy@gmail.com>
2023-02-15 18:05:33 +0100

    request http ok

--- srcs/parse_request_http.cpp
+++ srcs/parse_request_http.cpp
@@ -1,41 +1,22 @@
 # include <iostream>
 # include <map>
+# include <string>
+# include <vector>
+# include <sstream>
+# include <cstddef>
+# include "httpRequest.hpp"
 
-class HttpRequest
+void httpMessage(std::string const requestMsg)
 {
-	public:
-		HttpRequest(std::string request_method, std::string request_target, std::string protocol_version)
-		: _request_method(request_method), _request_target(request_target), _protocol_version(protocol_version){}
+	HttpRequest requestInfo(requestMsg);
+	std::string response;
 
-		void	setHeaderField(std::string field_name, std::string field_value){
-			_header_fields.insert(std::pair< std::string, std::string >(field_name, field_value));
-		}
-		void	setBody(std::string body){ _body = body; }
-
-		std::string const getRequestMethod() const { return _request_method;}
-		std::string const getRequestTarget() const { return _request_target;}
-		std::string const getProtocolVersion() const { return _protocol_version;}
-
-	private:
-		std::string const						_request_method;
-		std::string const						_request_target;
-		std::string const						_protocol_version;
-		std::map< std::string, std::string >	_header_fields;
-		std::string 							_body;
-};
-
-std::string parse_request_http(std::string request_msg)
-{
-	std::string request_method(request_msg, 0, request_msg.find(" "));
-	std::cout << request_method << std::endl;
-
-	// HttpRequest();
-	return (request_msg); //FAUX
+	
 }
 
 int main ()
 {
-	std::string request_msg = "\
+	std::string const requestMsg = "\
 GET /index.html HTTP/1.1\n\
 Host: localhost:8080\n\
 Connection: keep-alive\n\
@@ -45,7 +26,16 @@ User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n\
 DNT: 1\n\
 Accept-Encoding: gzip, deflate, br\n\
-Accept-Language: en-US,en;q=0.9\n";
+Accept-Language: en-US,en;q=0.9\n\
+\n\
+<html>\n\
+	<head>\n\
+		<title>Ma page : d'exemple</title>\n\
+	</head>\n\
+	<body>\n\
+	Voici le contenu\n\
+	</body>\n\
+</html>";
 
-	parse_request_http(request_msg);
+	httpMessage(requestMsg);
 }
