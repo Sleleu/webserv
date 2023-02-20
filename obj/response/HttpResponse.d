1e3d76a0df0ab0ef61f602096210192f133f555f
Eliot EDY <eedy@e2r11p19.clusters.42paris.fr>
2023-02-20 15:57:14 +0100

    update branch

---
+++ obj/response/HttpResponse.d
@@ -0,0 +1,13 @@
+obj/response/HttpResponse.o: src/response/HttpResponse.cpp \
+  src/response/../../header/response/HttpResponse.hpp \
+  src/response/../../header/response/HttpRequest.hpp \
+  src/response/../../header/response/response.hpp \
+  src/response/../../header/response/body.hpp
+
+src/response/../../header/response/HttpResponse.hpp:
+
+src/response/../../header/response/HttpRequest.hpp:
+
+src/response/../../header/response/response.hpp:
+
+src/response/../../header/response/body.hpp:
