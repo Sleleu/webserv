c7d49b86df79c5fe125641ac1714060e646d0bee
Eliot EDY <eedy@e2r11p19.clusters.42paris.fr>
2023-02-20 18:29:56 +0100

    v3 parser. Post parsing done (root/size/on_off)

--- src/parser/main.cpp
+++ src/parser/main.cpp
@@ -8,12 +8,18 @@ int main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 	
-	Parser parser(argv[1]);
-	// std::cout << parser << std::endl;
-	Server server = Server(parser.getBigVector()[0], parser.getBigLocation()[0]);
+	try
+	{
+		Parser parser(argv[1]);
+		// std::cout << parser << std::endl;
+		Server server = Server(parser.getBigVector()[0], parser.getBigLocation()[0]);
+		if (!server.init_server())
+			return (1);
+		server.start_server();
+	}
+	catch (std::exception &e)
+	{
+	}
 	
-	if (!server.init_server())
-		return (1);
-	server.start_server();
 	//simul_request(parser.getBigLocation()[0], parser.getBigVector()[0]);
 }
