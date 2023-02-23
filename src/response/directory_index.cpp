885288234503aa6a8b3cae5ca81ad0e6642cafd8
Eliot EDY <eedy@e2r9p17.clusters.42paris.fr>
2023-02-23 19:47:24 +0100

    add directory listing

---
+++ src/response/directory_index.cpp
@@ -0,0 +1,121 @@
+#include "../../header/response/directory_index.hpp"
+
+std::string	dir_list(char *path)
+{
+	DIR 						*dir = opendir(path);
+	struct dirent				*entitiy;
+	std::map<std::string, int>	file;
+	std::string					error;
+
+	if (dir == NULL) 
+	{
+		error = "the directory \"";
+		error += path;
+ 		error += "\" doesnt exist";
+
+		return (html_error(error));
+	}
+	entitiy = readdir(dir);
+	while (entitiy != NULL)
+	{
+		if (entitiy->d_type == DT_DIR)
+			file[entitiy->d_name] = _DIR;
+		else
+			file[entitiy->d_name] = _FILE;
+		entitiy = readdir(dir);
+	}
+	closedir(dir);
+	return (creat_html(file, path));
+}
+
+std::string	html_error(std::string error)
+{
+	std::string	html;
+
+html = "\n\
+<html>\n\
+ 	<head>\n\
+ 		<title>ERROR</title>\n\
+ 	</head>\n\
+ 	<body>\n\
+ 		<p>";
+	html += error;
+	html += "</p>\n\
+	</body>\n\
+<html>\n";
+	return (html);
+}
+
+std::string	creat_html(std::map<std::string, int> map, std::string path)
+{
+	std::string html;
+	
+	if (path[path.size() - 1] != '/')
+		path += "/";
+	path.erase(0, 6);
+html = "\n\
+<!DOCTYPE html>\n\
+<html>\n\
+ 	<head>\n\
+ 		<title>ERROR</title>\n\
+ 	</head>\n\
+ 	<body>\n\
+		<style>\n\
+			table, td {\n\
+    			border: 1px solid #333;\n\
+			}\n\
+			thead, tfoot {\n\
+    			background-color: #333;\n\
+    			color: #fff;\n\
+			}\n\
+			.blue {\n\
+				color:blue;\n\
+				text-decoration:underline;\n\
+			}\n\
+			.black {\n\
+				color:black;\n\
+				text-decoraton:none;\n\
+			}\n\
+			tr{\n\
+				text-align:center;\n\
+			}\n\
+			img{\n\
+				height:35px;\n\
+				width:35px;\n\
+			}\n\
+		</style>\n\
+		<table>\n\
+    		<thead>\n\
+        		<tr>\n\
+            		<th colspan=\"2\">Index</th>\n\
+        		</tr>\n\
+    		</thead>\n\
+    		<tbody>\n";
+	for (std::map<std::string, int>::iterator it = map.begin(); it != map.end(); it ++)
+	{
+		html += "        		<tr>\n";
+		html += "				<td><a class=\"";
+		if ((*it).second == _DIR)
+			html += "blue";
+		else
+			html += "black";
+		html += "\" href=\"";
+		html += path + (*it).first;
+		html += "\">";
+		html += (*it).first;
+		html += "</a></p></td>\n";
+		html += "				<td><img src=\"";
+		if ((*it).second == _DIR)
+			html += "/image/dir.png";
+		else
+			html += "/image/file.png";
+		html += "\" ></td>";
+		html += "      		</tr>\n";
+	}
+	html += "\
+    		</tbody>\n\
+		</table>\n\
+	</body>\n\
+<html>\n";
+	return (html);
+}
